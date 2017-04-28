24b9cb095b61efc15bf8756f51d50b4d2320835e
Rai Lee <thisisrailee@gmail.com>
2017-04-28 14:19:13 -0700

    Finished Simon Game.

--- script.js
+++ script.js
@@ -1,57 +1,64 @@
-// To work on after lunch. colorPicker does not stop.
-
-
-
 
 $(document).ready(
-// yellow = 0, blue = 1, red = 2, green = 3
   function() {
     let correctArr = []
     let playerArr = []
     let truthyflag = true
+    let strickFlag = false
+    let strictMode = false
     let stepCounter = correctArr.length
+    const sound0 = new Audio ('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3')
+    const sound1 = new Audio ('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3')
+    const sound2 = new Audio ('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3')
+    const sound3 = new Audio ('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
 
+    $('#checkbox').click(function(){
+      if(strictMode === false){
+        strictMode = true
+        stop()
+        $('#strictText').html('Strict Mode: on')
+      }else {
+        strictMode = false
+        stop()
+        $('#strictText').html('Strict Mode: off')
+      }
+    })
+
+    $('#start').click(function(){
+      onStartPress()
+    })
+
+    $('#stop').click(function() {
+      stop()
+    })
+
+    function stop() {
+      correctArr = []
+      playerArr = []
+      stepCounter = correctArr.length
+      $('#level').val(stepCounter)
+    }
+
+    function onStartPress() {
+      colorPicker()
+      displayColor()
+
+    }
+    //Picks random number to be pushed into correctArr
     function colorPicker() {
       let randomnumber = Math.floor(Math.random() * 4)
       correctArr.push(randomnumber)
+      stepCounter = correctArr.length
+      $('#level').val(stepCounter)
     }
 
-    function doSetTimeout(i){
-      setTimeout(function(){i}, 1000)
-    }
-
-    function displayColor() {
-      timedDisplayColor( correctArr.map( color => color ) )
-    }
-
+    //Array where each color is an object within an array.
     const COLORS = [
       { color: 'yellow', className: 'warning '},
       { color: 'blue', className: 'info '},
       { color: 'red', className: 'danger '},
       { color: 'green', className: 'success '},
     ]
-
-    function timedDisplayColor( colors ) {
-      const { color, className } = COLORS[ colors[ 0 ]]
-
-      $( `#${color}` ).removeClass( `btn-${className}` )
-
-      setTimeout( () => {
-        alternateTimedDisplayColor( colors )
-      }, 300 )
-    }
-
-    function alternateTimedDisplayColor( colors ) {
-      const { color, className } = COLORS[ colors.shift() ]
-
-      $( `#${color}` ).addClass( `btn-${className}` )
-
-      if( colors.length > 0 ) {
-        setTimeout( () => {
-          timedDisplayColor( colors )
-        }, 300 )
-      }
-    }
 
     COLORS.forEach( ({ color }, index ) => {
       $( `#${color}` ).click( () => {
@@ -60,18 +67,49 @@ $(document).ready(
         }
         if (playerArr.length === correctArr.length) {
           checker()
-          console.log('playerArr', playerArr);
-          if (truthyflag === true) {
+          if (truthyflag === true && strictMode === false) {
             playerArr = []
-            onStartPress()
-            console.log('playerArr', playerArr);
-          } else {
+            setTimeout(function(){onStartPress()}, 1000)
+          }
+          else if (truthyflag === false && strictMode === true) {
+            truthyflag = true
+            stop()
+            setTimeout(function(){onStartPress()}, 1000)
+          }
+          else if (truthyflag === true && strictMode === true ) {
             playerArr = []
-            displayColor()
+            setTimeout(function(){onStartPress()}, 1000)
+          }
+          else {
+            truthyflag = true
+            playerArr = []
+            setTimeout(function(){displayColor()}, 1000)
           }
         }
       })
     })
+
+    function displayColor() {
+      timedDisplayColor( correctArr.map( color => color ) )
+    }
+
+    function timedDisplayColor( colors ) {
+      const { color, className} = COLORS[ colors[ 0 ]]
+      $( `#${color}` ).removeClass( `btn-${className}` ).trigger('onclick')
+      setTimeout( () => {
+        alternateTimedDisplayColor( colors )
+      }, 500 )
+    }
+
+    function alternateTimedDisplayColor( colors ) {
+      const { color, className } = COLORS[ colors.shift() ]
+      $( `#${color}` ).addClass( `btn-${className}` )
+      if( colors.length > 0 ) {
+        setTimeout( () => {
+          timedDisplayColor( colors )
+        }, 500 )
+      }
+    }
 
     function checker () {
       for (let i = 0; i < playerArr.length; i++) {
@@ -80,18 +118,5 @@ $(document).ready(
         }
       }
     }
-    // If stuff breaks check this
 
-
-    function onStartPress() {
-      colorPicker()
-      displayColor()
-      console.log(correctArr);
-    }
-
-
-
-    $('#start').click(function(){
-      onStartPress()
-    })
   })
